8f9899d1572a2ec1673726c20a2b23647f655334
lijian <lij12@tcd.ie>
2019-07-06 05:11:19 +0100

    Merge branch 'adjust_OF_MSG'
    # Conflicts:
    #	.idea/workspace.xml
    #	controller.py
    #	controller_listener_ctrlinfo.py
    #	node.py

--- status/status_monitor.py
+++ status/status_monitor.py
@@ -65,6 +65,13 @@ class Status_Monitor(object):
         fib_status_record = Fib_status_getter().run()
         rib_status_record = Rib_status_getter().run()
         print('********************* Node Status Checking **********************')
+        print((self.channels_status_record != channels_status_record))
+        print(self.faces_status_record != faces_status_record)
+        print((self.fib_status_record != fib_status_record))
+        print(self.rib_status_record != rib_status_record)
+
+        print('********************* Node Status Checking **********************')
+
         '''anything changes will return True'''
         if (self.channels_status_record != channels_status_record) or \
                 (self.faces_status_record != faces_status_record) or \
