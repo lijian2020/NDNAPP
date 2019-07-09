1e5737ea3517e32cf1df851df019669e1debe93e
lijian <lij12@tcd.ie>
2019-07-10 00:30:04 +0100

    finished status monitor and trigger. almost finished all functions
    Add flapping protection function
    add face.shutdown

---
+++ testcontroller.py
@@ -0,0 +1,76 @@
+import sys
+import time
+import argparse
+import traceback
+import random
+
+from pyndn import Name
+from pyndn import Data
+from pyndn import Face
+from pyndn.security import KeyChain
+
+
+class Producer(object):
+    def __init__(self):
+        self.keyChain = KeyChain()
+        self.isDone = False
+
+    def run(self, namespace):
+        # Create a connection to the local forwarder over a Unix socket
+        face = Face()
+        prefix = Name(namespace)
+
+        # Use the system default key chain and certificate name to sign commands.
+        face.setCommandSigningInfo(self.keyChain, \
+                                   self.keyChain.getDefaultCertificateName())
+
+        # Also use the default certificate name to sign Data packets.
+        '''Register prefix with the connected NDN hub and call onInterest when a
+        matching interest is received. To register a prefix with NFD, you must
+        first call setCommandSigningInfo.'''
+        face.registerPrefix(prefix, self.onInterest, self.onRegisterFailed)
+
+        face.setInterestFilter(Name('/ndn/d-site/d/hello/ccc/ddd'), self.onInterest)
+
+        print("Registering prefix", prefix.toUri())
+
+        # Run the event loop forever. Use a short sleep to
+        # prevent the Producer from using 100% of the CPU.
+        while not self.isDone:
+            face.processEvents()
+            time.sleep(0.01)
+
+    def onInterest(self, prefix, interest, transport, registeredPrefixId):
+        print("--------received interest:" + interest.getName().toUri())  # for test
+        interestName = interest.getName()
+        appcontentstr = interest.getApplicationParameters()
+
+        data = Data(interestName)
+        data.setContent("Hello, " + interestName.toUri() + '--->>>' + appcontentstr.toRawStr() + '\n--->>>')
+
+        # hourMilliseconds = 3600 * 1000
+        hourMilliseconds = 0  # here I should set it 0 since it always need to fresh.
+
+        data.getMetaInfo().setFreshnessPeriod(hourMilliseconds)
+        self.keyChain.sign(data, self.keyChain.getDefaultCertificateName())
+        transport.send(data.wireEncode().toBuffer())
+        print("Replied to: %s" % interestName.toUri())
+
+    def onRegisterFailed(self, prefix):
+        print("Register failed for prefix", prefix.toUri())
+        self.isDone = True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parse command line args for ndn producer')
+    parser.add_argument("-n", "--namespace", required=True, help='namespace to listen under')
+
+    args = parser.parse_args()
+
+    try:
+        namespace = args.namespace
+        Producer().run(namespace)
+
+    except:
+        traceback.print_exc(file=sys.stdout)
+        sys.exit(1)
