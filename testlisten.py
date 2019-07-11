3756755843593283a63fdf25b18d8681677dd364
lijian <lij12@tcd.ie>
2019-07-11 23:14:28 +0100

    finished status monitor and trigger. almost finished all functions
    Add flapping protection function
    add face.shutdown

--- testlisten.py
+++ testlisten.py
@@ -1,17 +1,53 @@
 import os
 import time
+import datetime
+import pyinotify
+import logging
+
+
 
 pos = 0
-while True:
-    # fd = open(r'./abc.txt')
-    with open(r'./abc.txt') as fd:
-        if pos != 0:
-            fd.seek(pos, 0)
-        while True:
-            line = fd.readline()
-            if line.strip():
-                print(line.strip())
-            pos = pos + len(line)
-            if not line.strip():
-                break
-# fd.close()
+
+
+def printlog():
+    global pos
+    try:
+        with open(r'./abc.txt') as fd:
+            if pos != 0:
+                fd.seek(pos, 0)
+            while True:
+                line = fd.readline()
+                if line.strip():
+                    print(line.strip())
+                pos = pos + len(line)
+                if not line.strip():
+                    break
+    except:
+        print("error")
+
+
+class MyEventHandler(pyinotify.ProcessEvent):
+
+    # 当文件被修改时调用函数
+    def process_IN_MODIFY(self, event):
+        try:
+            printlog()
+        except:
+            print("error")
+
+
+def main():
+    # 输出前面的log
+    printlog()
+    # watch manager
+    wm = pyinotify.WatchManager()
+    wm.add_watch('./abc.txt', pyinotify.ALL_EVENTS, rec=True)
+    eh = MyEventHandler()
+
+    # notifier
+    notifier = pyinotify.Notifier(wm, eh)
+    notifier.loop()
+
+
+if __name__ == '__main__':
+    main()
