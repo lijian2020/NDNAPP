de9af5dc3bbbd99205b71cfc0d64156f34817a3e
lijian <lij12@tcd.ie>
2019-07-12 00:50:42 +0100

    finished status monitor and trigger. almost finished all functions
    Add flapping protection function
    add face.shutdown

--- testlisten.py
+++ testlisten.py
@@ -20,26 +20,44 @@ import logging
 # # fd.close()
 #
 
-pos = 0
-def printlog():
-    global pos
-    try:
-        fd = open(r'./abc.txt')
-        if pos != 0:
-            fd.seek(pos, 0)
-        while True:
-            line = fd.readline()
-            if line.strip():
-                print(line.strip())
-            pos = pos + len(line)
-            if not line.strip():
-                break
-        fd.close()
-    except:
-        print("error")
+# pos = 0
+# def printlog():
+#     global pos
+#     try:
+#         fd = open(r'./abc.txt')
+#         if pos != 0:
+#             fd.seek(pos, 0)
+#         while True:
+#             line = fd.readline()
+#             if line.strip():
+#                 print(line.strip())
+#             pos = pos + len(line)
+#             if not line.strip():
+#                 break
+#         fd.close()
+#     except:
+#         print("error")
 
 
 class MyEventHandler(pyinotify.ProcessEvent):
+    pos = 0
+
+    def printlog(self):
+        global pos
+        try:
+            fd = open(r'./abc.txt')
+            if pos != 0:
+                fd.seek(pos, 0)
+            while True:
+                line = fd.readline()
+                if line.strip():
+                    print(line.strip())
+                pos = pos + len(line)
+                if not line.strip():
+                    break
+            fd.close()
+        except:
+            print("error")
 
     # 当文件被修改时调用函数
     def process_IN_MODIFY(self, event):
@@ -51,7 +69,7 @@ class MyEventHandler(pyinotify.ProcessEvent):
 
 def main():
     # 输出前面的log
-    printlog()
+    # printlog()
     # watch manager
     wm = pyinotify.WatchManager()
     wm.add_watch('./abc.txt', pyinotify.ALL_EVENTS, rec=True)
