f262eb2cd2bbbab1160de184f89fafaa99c41f58
lijian <lij12@tcd.ie>
2019-07-11 23:06:57 +0100

    finished status monitor and trigger. almost finished all functions
    Add flapping protection function
    add face.shutdown

--- testlisten.py
+++ testlisten.py
@@ -3,20 +3,15 @@ import time
 
 pos = 0
 while True:
-    fd = open(r'./abc.txt')
-    '''
-    for line in fd:
-        print line.strip()
-
-    fd.close()
-    '''
-    if pos != 0:
-        fd.seek(pos, 0)
-    while True:
-        line = fd.readline()
-        if line.strip():
-            print(line.strip())
-        pos = pos + len(line)
-        if not line.strip():
-            break
-    fd.close()
+    # fd = open(r'./abc.txt')
+    with open(r'./abc.txt') as fd:
+        if pos != 0:
+            fd.seek(pos, 0)
+        while True:
+            line = fd.readline()
+            if line.strip():
+                print(line.strip())
+            pos = pos + len(line)
+            if not line.strip():
+                break
+# fd.close()
