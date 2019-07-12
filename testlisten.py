d711c0c70564b0f9acedd73276574152d84a397b
lijian <lij12@tcd.ie>
2019-07-12 01:09:33 +0100

    finished status monitor and trigger. almost finished all functions
    Add flapping protection function
    add face.shutdown

--- testlisten.py
+++ testlisten.py
@@ -5,69 +5,26 @@ import pyinotify
 import logging
 
 
-# pos = 0
-# while True:
-#     try:
-#         with open(r'./abc.txt') as fd:
-#             if pos != 0:
-#                 fd.seek(pos, 0)
-#             while True:
-#                 line = fd.readline()
-#                 if line.strip():
-#                     print(line.strip())
-#                     linestr = line.strip()
-#                     linelist = linestr.split()
-#                     print(linelist[0])
-#
-#                 pos = pos + len(line)
-#                 if not line.strip():
-#                     break
-#     except:
-#         print('error in open log file')
-#
-#
-
-
-class MyEventHandler(pyinotify.ProcessEvent):
-    def process_IN_MODIFY(self, event):
+def printline():
+    pos = 0
+    while True:
         try:
-            pos = 0
-            while True:
-                try:
-                    with open(r'./abc.txt') as fd:
-                        if pos != 0:
-                            fd.seek(pos, 0)
-                        while True:
-                            line = fd.readline()
-                            if line.strip():
-                                print(line.strip())
-                                linestr = line.strip()
-                                linelist = linestr.split()
-                                print(linelist[0])
+            with open(r'./abc.txt') as fd:
+                if pos != 0:
+                    fd.seek(pos, 0)
+                while True:
+                    line = fd.readline()
+                    if line.strip():
+                        print(line.strip())
+                        linestr = line.strip()
+                        linelist = linestr.split()
+                        print(linelist[0])
 
-                            pos = pos + len(line)
-                            if not line.strip():
-                                break
-                except:
-                    print('error in open log file')
-
-
+                    pos = pos + len(line)
+                    if not line.strip():
+                        break
         except:
-            print('error')
+            print('error in open log file')
 
 
-def main():
-    # watch manager
-    wm = pyinotify.WatchManager()
-    wm.add_watch('./abc.txt', pyinotify.ALL_EVENTS, rec=True)
-
-    # event handler
-    eh = MyEventHandler()
-
-    # notifier
-    notifier = pyinotify.Notifier(wm, eh)
-    notifier.loop()
-
-
-if __name__ == '__main__':
-    main()
+print()
