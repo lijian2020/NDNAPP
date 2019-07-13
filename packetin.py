c237882b7bc26c7db65c22f447c1e16df24df448
lijian <lij12@tcd.ie>
2019-07-13 07:08:34 +0100

    finished status monitor and trigger. almost finished all functions
    Add flapping protection function
    add face.shutdown

--- packetin.py
+++ packetin.py
@@ -90,10 +90,10 @@ class PacketIn(object):
         print("TIMEOUT #%d: %s" % (self.outstanding[uri], uri))
         self.outstanding[uri] += 1
 
-        if self.outstanding[uri] <= 3:
-            self.run()
-        else:
-            self.isDone = True
+        # if self.outstanding[uri] <= 3:
+        #     self.run()
+        # else:
+        #     self.isDone = True
 
 
 
